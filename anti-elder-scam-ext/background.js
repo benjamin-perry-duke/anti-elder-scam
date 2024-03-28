35052155359123e920653f53b8a88ed707959fcf
Ben Perry <benjamin.perry.nc@gmail.com>
2024-03-28 12:39:06 -0400

    fixing manifest version

--- anti-elder-scam-ext/background.js
+++ anti-elder-scam-ext/background.js
@@ -1,5 +1,8 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.status === 'complete') {
-      chrome.tabs.executeScript(tabId, { file: 'content.js' });
-    }
-  });
+  if (changeInfo.status === 'complete') {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      files: ['content.js']
+    });
+  }
+});
