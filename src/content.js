44562f10e56a763dcb47546158320ce4f613872f
Ben Perry <benjamin.perry.nc@gmail.com>
2024-04-28 21:48:03 -0400

    using npm install

---
+++ src/content.js
@@ -0,0 +1,13 @@
+// content.js - the content scripts which is run in the context of web pages, and has access
+// to the DOM and other web APIs.
+
+// Example usage:
+// const message = {
+//     action: 'classify',
+//     text: 'text to classify',
+// }
+// chrome.runtime.sendMessage(message, (response) => {
+//     console.log('received user data', response)
+// });
+
+// Currently, we are not using this file, but it is included here for reference.
