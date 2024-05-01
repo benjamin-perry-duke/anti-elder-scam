c793c69876cc350815273d6a2bc3e418af2d10d7
Benjamin Perry <benjamin.perry.nc@gmail.com>
2024-04-30 23:02:11 -0400

    final version

--- anti-elder-scam-ext/background.js
+++
@@ -1,8 +0,0 @@
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete') {
-    chrome.scripting.executeScript({
-      target: { tabId: tabId },
-      files: ['content.js']
-    });
-  }
-});
